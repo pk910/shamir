89c1726c5250a027d56ed20d33da29a1e1f293da
Aubrey Oosthuizen <aubrey.oosthuizen@blockchain-consulting.net>
2019-07-22 22:34:01 +0200

    generate static site

--- webpack.config.common.js
+++ webpack.config.common.js
@@ -13,7 +13,7 @@ const CopyWebpackPlugin = require('copy-webpack-plugin');
 module.exports = {
   entry: './src/app.js',
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'docs'),
     filename: '[name].[hash].js'
   },
   optimization: {
