66f7680c1c715c98f4d0df14061b440557eb9b7e
pk910 <philipp@pk910.de>
2025-01-11 17:37:17 +0100

    update dependencies, use `pkg` for executables

--- webpack.config.common.js
+++ webpack.config.common.js
@@ -3,9 +3,6 @@ const path = require('path');
 const webpack = require('webpack');
 
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const { NexePlugin } = require('nexe-webpack-plugin');
-
-const nexeUpxPlugin = require('./utils/nexe-upx-plugin');
 
 var buildTemp = path.join(__dirname, "build");
 
@@ -22,24 +19,6 @@ module.exports = (devBuild) => {
     },
     plugins: [
       new CleanWebpackPlugin(),
-      new NexePlugin([
-        {
-          input: 'shamir39.js',
-          temp: buildTemp,
-          arch: 'win32-x64',
-          output: 'shamir39-win32.exe',
-          target: 'windows-x64-14.15.3',
-          plugins: [ nexeUpxPlugin(!devBuild) ]
-        },
-        {
-          input: 'shamir39.js',
-          temp: buildTemp,
-          arch: 'linux-x64',
-          output: 'shamir39-linux',
-          target: 'linux-x64-14.15.3',
-          plugins: [ nexeUpxPlugin(!devBuild) ]
-        },
-      ]),
     ]
   };
 };
